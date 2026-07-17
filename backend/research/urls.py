7c1febeba71854640215a8b638835b34731c2091
Hina Ayub <hinaayub2002@gmail.com>
2026-07-17 20:36:59 +0500

    Remove frontend and backend folders from main

--- backend/research/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ResearchView, ResearchHistoryView
-
-urlpatterns = [
-    path("research/", ResearchView.as_view(), name="research"),
-    path("research/history/", ResearchHistoryView.as_view(), name="research-history"),
-]
